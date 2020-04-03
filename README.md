73031b253e7db65a648ea49cba0ce96f7e65c420
Jon Jaryong Lee <director9303@gmail.com>
2020-04-03 17:53:34 +0900

    0403

--- README.md
+++ README.md
@@ -28,3 +28,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 4월 3일
+vuetify에서 이미 만들어져있던 코드인 complex 레이아웃 적용
+메인화면 작업 시작
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,73 @@
 <template>
-  <div>
-    Hello AccountBook
-  </div>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawerRight" app clipped right>
+            <v-list dense>
+                <v-list-item @click.stop="right = !right">
+                    <v-list-item-action>
+                        <v-icon>mdi-exit-to-app</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app clipped-right color="teal lighten-2">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <!-- 툴바엔 몇 월이 들어갈지 나옴 -->
+            <v-toolbar-title class="month">
+                dd
+            </v-toolbar-title>
+            <v-spacer />
+            <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight" />
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense>
+                <v-list-item @click.stop="left = !left">
+                    <v-list-item-action>
+                        <v-icon>mdi-exit-to-app</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-navigation-drawer v-model="left" fixed temporary />
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row justify="center" align="center">
+                    <!-- 한가운데 -->
+                    한가운데
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-navigation-drawer v-model="right" fixed right temporary />
+        <v-footer app>
+            <span>footer</span>
+        </v-footer>
+    </v-app>
 </template>
+<script>
+export default {
+    props: {
+        source: String,
+    },
+    data: () => ({
+        drawer: null,
+        drawerRight: null,
+        right: false,
+        left: false,
+    }),
+}
+</script>
+<style>
+.month {
+    position: relative;
+    left: 80px;
+}
+
+.footer {
+    border: 1px solid black;
+}
+</style>
